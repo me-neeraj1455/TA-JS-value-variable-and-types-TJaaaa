407303296442cc396c4a8ffeef6562480b5951e9
Neeraj-kumar1411 <welcometoneeraj@gmail.com>
2021-12-24 00:07:53 +0530

    variable assignment2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,9 +6,24 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt(`Enter your name`);
+alert(user);
+
+let userName = user;
+console.log(user, userName);
+let age = 22,
+  isLearning = true;
+
+user = "John";
+console.log(user, "user", userName, "username");
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = Number(prompt(`Enter first number`));
+let numB = Number(prompt(`Enter second number`));
+let sum = numA + numB;
+alert(sum);
 
 // Loops
 
@@ -22,22 +37,36 @@ for (let j = 0; j <= 10; j = j + 1) {
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
 for (let i = 9; i >= 1; i = i - 1) {
-  console.log(i);
+  if (i % 2 != 0) {
+    console.log(i);
+  }
 }
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let concat = "";
 let initialValue = 5;
-while (initialValue >= 1) {
-  initialValue = initialValue - 1;
-  console.log(concat);
+while (initialValue >= 0) {
+  concat = concat + initialValue;
+  initialValue--;
 }
+console.log(concat);
+
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
-let;
+let string = "";
+let numbers = 10;
+while (numbers >= 0) {
+  if (numbers % 2 == 0) {
+    string += numbers;
+  }
+  numbers--;
+}
+console.log(string);
 
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
+
 
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
@@ -49,3 +78,11 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = +prompt(`Enter first num`);
+let num2 = +prompt(`Enter second num`);
+if (isNaN(num1) || isNaN(num2)) {
+  alert(`Enter a valid value`);
+} else {
+  alert(num1 === num2);
+}
